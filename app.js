e5f1ccff6be0cd445903d78ee10d2ae1bcdf4c23
prathammanjul <prathammanjul91@gmail.com>
2026-04-02 21:37:23 +0530

    feat: implement authentication system with Passport.js, including login, sessions, and flash messaging

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 app.use(express.static("public"));
 
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const flash = require("connect-flash");
+
 // 🔗 DATABASE CONNECTION
 mongoose
   .connect("mongodb://127.0.0.1:27017/interviewPrepDB")
@@ -16,6 +22,30 @@ mongoose
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+
+//session middleware
+app.use(
+  session({
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+//intialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// middleware
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // Route
 app.get("/", (req, res) => {
@@ -26,6 +56,41 @@ app.get("/", (req, res) => {
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
+
+// submit signup form
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const newUser = new User({ username, email });
+    await User.register(newUser, password);
+
+    req.flash("success", "Welcome! Account created successfully 🎉");
+    res.redirect("/login");
+  } catch (err) {
+    req.flash("error", err.message);
+    res.redirect("/signup");
+  }
+});
+
+//login route
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+// post login
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back! Logged in successfully 🎉");
+    res.redirect("/");
+  },
+);
 
 // Server
 app.listen(3000, () => {
